8e09405fda9d9f1fd681d9a8ee503829891ae2e1
Abhiram Mishra <aneonoir@gmail.com>
2020-03-27 21:23:07 +0530

    update

--- Arrays Level 1/sum13/src/Task.java
+++ Arrays Level 1/sum13/src/Task.java
@@ -1,5 +1,13 @@
 public class Task {
     public int sum13(int[] nums) {
-
+        int sum=0;
+        if(nums.length==0) return sum;
+        int last=nums[0];
+        sum=last==13 ? 0: last;
+        for (int i=1;i<nums.length;i++) {
+            if(last==13 || nums[i]==13 ) {last=nums[i];continue;}
+            else {sum+=nums[i];last=nums[i];}
+        }
+        return sum;
     }
 }
--- Arrays Level 1/sum13/test/Tests.java
+++ Arrays Level 1/sum13/test/Tests.java
@@ -1,10 +1,26 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class Tests {
-  @Test
-  public void testSolution() {
-    // put your test here
-    Assert.fail("Tests not implemented for the task");
-  }
+    @Test
+    public void testSolution() {
+
+        Task t = new Task();
+        assertEquals(6, t.sum13(new int[]{1, 2, 2, 1}));
+        assertEquals(2, t.sum13(new int[]{1, 1}));
+        assertEquals(6, t.sum13(new int[]{1, 2, 2, 1, 13}));
+        assertEquals(4, t.sum13(new int[]{1, 2, 13, 2, 1, 13}));
+        assertEquals(3, t.sum13(new int[]{13, 1, 2, 13, 2, 1, 13}));
+        assertEquals(0, t.sum13(new int[]{}));
+        assertEquals(0, t.sum13(new int[]{13}));
+        assertEquals(0, t.sum13(new int[]{13, 13}));
+        assertEquals(0, t.sum13(new int[]{13, 0, 13}));
+        assertEquals(0, t.sum13(new int[]{13, 1, 13}));
+        assertEquals(14, t.sum13(new int[]{5, 7, 2}));
+        assertEquals(5, t.sum13(new int[]{5, 13, 2}));
+        assertEquals(0, t.sum13(new int[]{0}));
+        assertEquals(0, t.sum13(new int[]{13,0}));
+    }
 }
