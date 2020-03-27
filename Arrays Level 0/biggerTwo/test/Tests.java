e56650ccb3777f935baa5bdbe6aa5be785007892
Abhiram Mishra <aneonoir@gmail.com>
2020-03-27 21:53:35 +0530

    updated another question

--- Arrays Level 0/biggerTwo/test/Tests.java
+++ Arrays Level 0/biggerTwo/test/Tests.java
@@ -5,6 +5,14 @@ public class Tests {
   @Test
   public void testSolution() {
     // put your test here
-    Assert.fail("Tests not implemented for the task");
+
+    /*biggerTwo([1, 2], [3, 4]) → [3, 4]	[3, 4]	OK
+biggerTwo([3, 4], [1, 2]) → [3, 4]	[3, 4]	OK
+biggerTwo([1, 1], [1, 2]) → [1, 2]	[1, 2]	OK
+biggerTwo([2, 1], [1, 1]) → [2, 1]	[2, 1]	OK
+biggerTwo([2, 2], [1, 3]) → [2, 2]	[2, 2]	OK
+biggerTwo([1, 3], [2, 2]) → [1, 3]	[1, 3]	OK
+biggerTwo([6, 7], [3, 1]) → [6, 7]	[6, 7]	OK
+*/
   }
 }
