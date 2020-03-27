8c6200889d10cbe17da291a3ef947a1b148cef60
Abhiram Mishra <aneonoir@gmail.com>
2020-03-27 21:00:04 +0530

    Updated question

--- Arrays Level 1/countEvens/src/Task.java
+++ Arrays Level 1/countEvens/src/Task.java
@@ -1,3 +1,9 @@
 public class Task {
     public int countEvens(int[] nums) {
+        int count=0;
+        for (Integer in : nums) {
+            if(in%2==0) count++;
+        }
+        return count;
+    }
 }
--- Arrays Level 1/countEvens/test/Tests.java
+++ Arrays Level 1/countEvens/test/Tests.java
@@ -1,10 +1,18 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class Tests {
   @Test
   public void testSolution() {
-    // put your test here
-    Assert.fail("Tests not implemented for the task");
+
+    Task t= new Task();
+    assertEquals(3, t.countEvens(new int[]{2, 1, 2, 3, 4}));
+    assertEquals(3, t.countEvens(new int[]{2, 2,0}));
+    assertEquals(1, t.countEvens(new int[]{1,2,5}));
+    assertEquals(3, t.countEvens(new int[]{2,2,2}));
+    assertEquals(3, t.countEvens(new int[]{0,0,0}));
+    assertEquals(0, t.countEvens(new int[]{1,3,5}));
   }
 }
