a888d200912a45e560725a7bf8293a36711f8f28
aneonoir <aneonoir@gmail.com>
2020-03-28 16:07:16 +0530

    some code and some utility

---
+++ Sorting/Insertion Sort/Insertion Sort/src/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    public void sortInsertion(int [] arr){
+        for(int i=1;i<arr.length;i++){
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && temp<arr[j] ){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
+}
